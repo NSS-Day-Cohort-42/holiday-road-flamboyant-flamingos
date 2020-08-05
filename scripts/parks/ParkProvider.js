87ca9cac788fcfd513de74f193dbd5a470d0d70f
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-08-05 14:29:50 -0500

    Parks dropdown functional

--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -8,6 +8,6 @@ export const getParks = () => {
     return fetch("https://developer.nps.gov/api/v1/parks?api_key=NCmlA6czOzmcPgcxI0P7hY53m2cLaepsPU0fS9k2")
         .then(response => response.json())
         .then(parsedParks => {
-            parks = parsedParks
+            parks = parsedParks.data
         })
 }
