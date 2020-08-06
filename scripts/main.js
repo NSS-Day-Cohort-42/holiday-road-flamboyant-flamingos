91bb15bef06ab53c6e00d5a8c1e3650fdfd9d642
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-06 00:09:28 -0500

    messing with index; also pulled in/console logged weather data

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,15 @@
 import {parksSelect} from "./parks/ParkSelect.js"
 import { AttractionSelect } from "./attractions/AttractionSelect.js";
 import { EaterySelect } from "./eateries/EaterySelect.js";
+import { getWeather, useWeatherCopy } from "./weather/WeatherProvider.js";
 
 EaterySelect()
 AttractionSelect()
 parksSelect()
+
+
+getWeather()
+    .then(() => {
+        const weatherDataTest = useWeatherCopy()
+        console.log(weatherDataTest)
+    })
