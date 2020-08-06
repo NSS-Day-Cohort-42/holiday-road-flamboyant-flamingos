cc84b5c7e197d081c7b218d37c52522beca07478
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-06 15:31:23 -0500

    about to checkout

--- scripts/weather/ForecastCard.js
+++ scripts/weather/ForecastCard.js
@@ -1,7 +1,6 @@
 export const forecastCard = currentParkForecastObject => {
     return `<div class="card forecastCard">
                 <div class="forecastCard__date">${new Date(currentParkForecastObject.dt).toLocaleDateString("en-US")}</<div>
-                <div 
             </div>
             `
 }
