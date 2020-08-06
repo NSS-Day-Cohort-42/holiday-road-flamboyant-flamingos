11fec80c0a4ec6393a742f057f2c0417a0833f62
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-08-06 13:21:59 -0500

    all selects are not connected to Itinerary Preview through custom events

---
+++ scripts/eateries/EateryItineraryPreview.js
@@ -0,0 +1,31 @@
+import { useEateries, getEateries } from "./EateryProvider.js";
+
+const contentTarget = document.querySelector(".eatery__preview")
+const eventHub = document.querySelector(".container")
+
+eventHub.addEventListener("eaterySelected", changeEvent => {
+    const eateryThatWasSelected = changeEvent.detail.businessName
+    getEateries()
+        .then(() => {
+            const arrayOfEateries = useEateries()
+            const foundEateryObj = arrayOfEateries.find(
+                (eateryObj) => {
+                    return eateryThatWasSelected === eateryObj.businessName
+        }
+    )
+    render(foundEateryObj)
+})
+})
+
+
+const render = (eatery) => {
+    contentTarget.innerHTML = `
+    <h3 class="previewHeading eatery__previewHeading">Current Eatery</h3>
+        <div class="eatery__name">${eatery.businessName}</div>
+        <button id="eateryDetailsButton"> Click for Details </button>
+        <dialog class= "eateryDialog--${eatery.id}">
+            <button id"eateryDetailCloseButton"></button>
+        </dialog>
+    `
+}
+
