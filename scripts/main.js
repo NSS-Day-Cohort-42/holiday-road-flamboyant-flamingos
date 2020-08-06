dbdbe63ed424fe0fb1f42d10cbad0313b39e4b9f
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-06 11:02:37 -0500

    forecastcard.js, continue buildout for forecast.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,12 @@ import {parksSelect} from "./parks/ParkSelect.js"
 import { AttractionSelect } from "./attractions/AttractionSelect.js";
 import { EaterySelect } from "./eateries/EaterySelect.js";
 import { getWeather, useWeatherCopy } from "./weather/WeatherProvider.js";
+import { Forecast } from "./weather/Forecast.js";
 
 EaterySelect()
 AttractionSelect()
 parksSelect()
+Forecast()
 
 
 getWeather()
--- scripts/weather/Forecast.js
+++ scripts/weather/Forecast.js
@@ -4,9 +4,13 @@ const render = (forecastData) => {
     contentTarget.innerHTML = `
                             <section>
                                 <h3 class="forecastHeading">5-day Forecast for this Park Destination</h3>
+                                ${
+                                    forecastData.map(forecastObj => {
+                                        return forecastCard(forecastObj)
+                                    })
+                                }
                             </section>
                             `
-
 } 
 
 export const Forecast = () => {
