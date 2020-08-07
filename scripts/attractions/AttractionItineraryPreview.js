e6af061e8f1076160ec96284d7814908bcb1a489
LukeZvada <zvadamusic@gmail.com>
2020-08-07 11:02:29 -0500

    buttons styled

--- scripts/attractions/AttractionItineraryPreview.js
+++ scripts/attractions/AttractionItineraryPreview.js
@@ -22,9 +22,9 @@ const render = (attraction) => {
     contentTarget.innerHTML = `
     <h3 class="previewHeading attraction__previewHeading">Current Attraction</h3>
         <div class="eatery__name">${attraction.name}</div>
-        <button id="eateryDetailsButton"> Click for Details </button>
+        <button id="attractionDetailsButton"> Click for Details </button>
         <dialog class= "eateryDialog--${attraction.id}">
-            <button id"eateryDetailCloseButton"></button>
+            <button id"attractionDetailCloseButton"></button>
         </dialog>
     `
 }
