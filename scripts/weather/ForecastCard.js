52673c776468d1be1c4b82e3ad2ec2671ce1dbd2
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-06 19:28:36 -0500

    importing icons by code

--- scripts/weather/ForecastCard.js
+++ scripts/weather/ForecastCard.js
@@ -4,12 +4,14 @@ export const forecastCard = currentDayArray => {
     // const hiTemp = 
     // const lowTemp =
     let conditions = currentDayArray[3].weather[0].main //Condition at 12 Noon
-    let date = new Date(currentDayArray[0].dt * 1000).toLocaleString("en-US", {weekday: "long"})
+    let dayOfWeek = new Date(currentDayArray[0].dt * 1000).toLocaleString("en-US", {weekday: "long"})
+    let icon = currentDayArray[3].weather[0].icon
     // let hiTemp = currentDayArray
 
     return `<div class="card forecastCard">
-                <div>${date}</div>
+                <div>${dayOfWeek}</div>
                 <div class="forecastCard__date">${conditions}</<div>
+                <img class="forecastCard__icon" src="http://openweathermap.org/img/w/${icon}.png">
             </div>
             `
 }
