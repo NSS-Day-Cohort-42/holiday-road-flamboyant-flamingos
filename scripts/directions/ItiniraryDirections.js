33470b2e745cd5599732597e334928d49dd08b8e
Gavin Schriver <alexanderschriver@gmail.com>
2020-08-08 15:07:20 -0500

    um

--- scripts/directions/ItiniraryDirections.js
+++ scripts/directions/ItiniraryDirections.js
@@ -1,58 +1,72 @@
-import { getParks, useParksCopy} from "../parks/ParkProvider.js"
-import { getAttractions, useAttractions } from "../attractions/AttractionProvider.js"
-import { getEateries, useEateries } from "../eateries/EateryProvider.js"
+import { getParks, useParksCopy } from "../parks/ParkProvider.js";
+import {
+  getAttractions,
+  useAttractions,
+} from "../attractions/AttractionProvider.js";
+import { getEateries, useEateries } from "../eateries/EateryProvider.js";
 
-const eventHub = document.querySelector(".container")
+const eventHub = document.querySelector(".container");
 
-eventHub.addEventListener("click", clickEvent => {
-    if (clickEvent.target.className === "getDirections") {
-        const [prefix, directionsParkName, directionsAttractionName, directionsEateryName] = clickEvent.target.parentNode.id.split("--")
+eventHub.addEventListener("click", (clickEvent) => {
+  if (clickEvent.target.className === "getDirections") {
+    const [
+      prefix,
+      directionsParkName,
+      directionsAttractionName,
+      directionsEateryName,
+    ] = clickEvent.target.parentNode.id.split("--");
 
+    getEateries()
+        .then(() => {
+      getAttractions()
+        .then(() => {
+          const attractionsArray = useAttractions();
+          const matchingAttraction = attractionsArray.find((attractionObj) => {
+            return attractionObj.name === directionsAttractionName;
+          });
 
-        getEateries()
-                .then(() => {
-                    const eateriesArray = useEateries()
-                    const matchingEatery
-                    
-                    
-                    getAttractions()
-                    .then(() => {
-                        const attractionsArray = useAttractions()
-                        const matchingAttraction = attractionsArray.find( attractionObj => {
-                            return (attractionObj.name === directionsAttractionName)
-                        })
-                        
-                        const attractionLocation = matchingAttraction.city + matchingAttraction.state
-                        console.log(attractionLocation)
-                        return attractionLocation
-                    }).then(() => { 
-                        
-                        
-                        getParks()
-                        .then(() => {
-                            const parksArray = useParksCopy()
-                            
-                            const matchingPark = parksArray.find(parkObj => {
-                                return (parkObj.name === directionsParkName)
-                            })
-                            
-                            const matchingParkLat = matchingPark.latitude
-                            const matchingParkLong = matchingPark.longitude
-                            
-                            const getDirectionsButtonEvent = new CustomEvent ("getDirectionsButtonPressed", {
-                                detail: {
-                                    parkLat: matchingParkLat,
-                                    parkLong: matchingParkLong,
-                                    attractionName: directionsAttractionName,
-                                    eateryName: directionsEateryName
-                                }
-                            })
-                            
-                            eventHub.dispatchEvent(getDirectionsButtonEvent)
-                        }) // close .then
-                    }) // close .then
-                }) // close .then 
-                    
-                } //end if for event listener condition
-}) //end listener 
+          const attractionLocation =
+            matchingAttraction.city + matchingAttraction.state;
+          console.log(attractionLocation);
+          return attractionLocation;
+        })
+        .then(() => {
+          getParks().then(() => {
+            const parksArray = useParksCopy();
 
+            const matchingPark = parksArray.find((parkObj) => {
+              return parkObj.name === directionsParkName;
+            });
+
+            const matchingParkLat = matchingPark.latitude;
+            const matchingParkLong = matchingPark.longitude;
+
+            const getDirectionsButtonEvent = new CustomEvent(
+              "getDirectionsButtonPressed",
+              {
+                detail: {
+                  parkLat: matchingParkLat,
+                  parkLong: matchingParkLong,
+                  attractionName: directionsAttractionName,
+                  eateryName: directionsEateryName,
+                },
+              }
+            );
+
+            eventHub.dispatchEvent(getDirectionsButtonEvent);
+          }); // close .then
+        });
+    }); // close .then
+  } //end if for event listener condition
+}); //end listener
+
+// getEateries()
+// .then(() => {
+//     const eateriesArray = useEateries()
+//     const matchingEatery = eateriesArray.find( eateryObj => {
+//         return (eateryObj.name === directionsEateryName)
+//     })
+
+//     const eateryLocation = matchingEatery.city + matchingEatery.state
+//     console.log (eateryLocation)
+// }).then(() =>
