bc48d7d937d5144211eb863a1d094012b48ad340
samanthajmaas <samanthajmaas@gmail.comcdclear>
2020-08-10 10:48:19 -0500

    some styling

--- scripts/savedItineraries/Itinerary.js
+++ scripts/savedItineraries/Itinerary.js
@@ -1,9 +1,9 @@
 export const itinerary = (itineraryObject) => {
     return `
         <section id="getDirectionsFor--${itineraryObject.parkName}--${itineraryObject.attractionName}--${itineraryObject.eateryName}" class="card itineraryCard">
-            <div class="itineraryItem itineraryItem__park">${itineraryObject.parkName}</div>
-            <div class="itineraryItem itineraryItem__attraction">${itineraryObject.attractionName}</div>
-            <div class="itineraryItem itineraryItem_eatery">${itineraryObject.eateryName}</div>
+            <div class="itineraryItem itineraryItem__park">Park: ${itineraryObject.parkName}</div>
+            <div class="itineraryItem itineraryItem__attraction">Attraction: ${itineraryObject.attractionName}</div>
+            <div class="itineraryItem itineraryItem_eatery">Eatery: ${itineraryObject.eateryName}</div>
             <button class="getDirections">Get Directions</button>
             <button class="deleteItinerary" id="delete--${itineraryObject.id}">Delete Intinerary</button>
         </section>
